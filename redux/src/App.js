7201ad63954ec164736180f7f192d7da8df599ee
socr102 <normanburtonfree@gmail.com>
2022-04-13 08:00:07 +0900

    redux

---
+++ redux/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './container/appContainer.js';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
